29d3297d3ddefc4ae1123d523e277e92c83d1978
Paul Varache <varache.paul@gmail.com>
2014-09-17 11:24:34 +0200

    svn_config is now a multi task

--- tasks/svn_config.js
+++ tasks/svn_config.js
@@ -13,7 +13,7 @@ module.exports = function (grunt) {
     // Please see the Grunt documentation for more information regarding task
     // creation: http://gruntjs.com/creating-tasks
 
-    grunt.registerTask('svn_config', 'Set the connexion vars', function () {
+    grunt.registerMultiTask('svn_config', 'Set the connexion vars', function () {
         var options = this.options({
             bin:         'svn',
             repository:  '',
@@ -21,6 +21,7 @@ module.exports = function (grunt) {
             execOptions: {},
             svnOptions: {}
         });
+
         grunt.config.set("svn_options", options);
     });
 };
